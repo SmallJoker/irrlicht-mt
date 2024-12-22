55a3a7217405139515ae6aaeb7e1acc371808e2f
DS <ds.desour@proton.me>
2024-12-22 09:28:44 +0100

    [MT] Cleanup headers in include/
    Backported by SmallJoker. Origin: Minetest. Hash: e5d321d28, Date: 2024-10-18

--- src/Irrlicht.cpp
+++ src/Irrlicht.cpp
@@ -12,6 +12,9 @@ static const char *const copyright = "Irrlicht Engine (c) 2002-2017 Nikolaus Geb
 #endif
 
 #include "irrlicht.h"
+#include "matrix4.h"
+#include "SMaterial.h"
+
 #ifdef _IRR_COMPILE_WITH_WINDOWS_DEVICE_
 #include "CIrrDeviceWin32.h"
 #endif
