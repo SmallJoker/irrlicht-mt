ab44e8a22b62ed6a22c120abaf068af47bbf4e4a
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2025-10-12 11:26:50 +0200

    [LT] Fix a memory leak: Use irr_ptr for bones
    Backported commit. Origin: Luanti. Hash: 4238aa423, Date: 2025-10-01

--- src/CAnimatedMeshSceneNode.h
+++ src/CAnimatedMeshSceneNode.h
@@ -10,6 +10,7 @@
 
 #include "SkinnedMesh.h"
 #include "Transform.h"
+#include "irrPtr.h"
 #include "matrix4.h"
 
 namespace irr
@@ -174,7 +175,7 @@ private:
 	std::function<void(f32)> OnAnimateCallback;
 
 	struct PerJointData {
-		std::vector<CBoneSceneNode *> SceneNodes;
+		std::vector<irr_ptr<CBoneSceneNode>> SceneNodes;
 		std::vector<core::matrix4> GlobalMatrices;
 		std::vector<std::optional<core::Transform>> PreTransSaves;
 		void setN(u16 n) {
